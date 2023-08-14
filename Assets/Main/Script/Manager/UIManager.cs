ffb1559319edfc5d3b1804f530d6569c2c2325df
DkyD1210 <harang0512@naver.com>
2023-08-14 19:53:10 +0900

    2023.08.14

--- Assets/Main/Script/Manager/UIManager.cs
+++ Assets/Main/Script/Manager/UIManager.cs
@@ -10,16 +10,27 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI m_CostText;
 
-    public List<GameObject> m_Hand;
+    [SerializeField]
+    private RectTransform CardLayer;
 
+    public List<Card> m_CardList;
+
+    [SerializeField]
+    private Vector3 HandStart;
+
+    [SerializeField]
+    private Vector3 HandEnd;
 
 
     void Start()
     {
         battleManager = BattleManager.Instance;
+        m_CardList.AddRange(CardLayer.gameObject.GetComponentsInChildren<Card>());
+        HandStart = new Vector3((CardLayer.rect.width / 2) * -1, 0, 0);
+        HandEnd = new Vector3(CardLayer.rect.width / 2, 0, 0);
     }
 
-    
+
     void Update()
     {
         ShowCost();
@@ -30,12 +41,31 @@ public class UIManager : MonoBehaviour
     {
         string maxcost = battleManager.GetMaxCost().ToString();
         string cost = battleManager.GetCost().ToString();
-        
+
         m_CostText.text = $"{cost}/{maxcost}";
     }
 
     private void CardHand()
     {
 
+        int count = m_CardList.Count;
+
+
+
+
+        for (int i = 0; i < count; i++)
+        {
+            //float distance = Vector3.Distance(HandStart, HandEnd);
+            float value = (float)i / (count - 1);
+            Vector3 pos = Vector3.Lerp(HandStart, HandEnd, value);
+            pos.y = CardLayer.position.y * (Mathf.Abs(0.5f - value) * -1);
+
+            Card card = m_CardList[i];
+
+            card.transform.localPosition = pos;
+        }
     }
+
+
+
 }
